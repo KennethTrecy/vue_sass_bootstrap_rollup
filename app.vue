b7b34cac62fe20511a1ec8d26e212e430d0cc6b9
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-07-18 10:38:33 +0800

    Create sample app

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+	<h1>{{ message }}</h1>
+	<h2>{{ message }}</h2>
+</template>
+
+<style lang="scss">
+h1 {
+	color: red;
+	+ h2 {
+		color: blue;
+	}
+}
+</style>
+
+<script>
+export default {
+	data() {
+		return {
+			"message": "Hello World"
+		}
+	}
+}
+</script>
